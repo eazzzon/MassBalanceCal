1e94097c7ecab570c7e46f9ee37658a4a9600edd
Eason Zhang <yishen.zhang@kuleuven.be>
2024-07-18 00:46:52 -0500

    check updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,22 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('./../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -40,6 +56,7 @@ extensions = ['sphinx.ext.autodoc',
 napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_ivar = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -60,3 +77,4 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
