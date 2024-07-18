e976fba6e249adb8b54f6df575502f422f56ae6e
Eason Zhang <yishen.zhang@kuleuven.be>
2024-07-18 01:09:25 -0500

    check updates

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 import setuptools
-setuptools.setup(pbr=True)
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
 
 setuptools.setup(
     name="massbalance",
@@ -12,17 +11,15 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/eazzzon/MassBalanceCal",
-    packages= setuptools.find_packages(
-        # exclude= ['example', 'benchmark', 'Tutorial']
-        ),
-
-
+    packages=setuptools.find_packages(
+        include=['example', 'benchmark', 'Tutorial', 'massbalance', 'massbalance.*', 'Tutorial.*']
+    ),
     install_requires=[
-            'pandas>=1.3.5',
-            'numpy>=1.21.5',
-            'matplotlib>=3.5.1',
-            'scipy>=1.7.3',
-            ],
+        'pandas>=1.3.5',
+        'numpy>=1.21.5',
+        'matplotlib>=3.5.1',
+        'scipy>=1.7.3',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
