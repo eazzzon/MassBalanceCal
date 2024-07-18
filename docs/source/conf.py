7a308c418597c10d0f9b210982b9bf12c49890ba
Eason Zhang <yishen.zhang@kuleuven.be>
2024-07-18 01:27:16 -0500

    check updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,8 +41,6 @@ author = 'Yishen Zhang'
 release = 'v0.1.4'
 
 
-# -- General configuration ---------------------------------------------------
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -72,11 +70,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-# html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-
+html_static_path = ['_static']
