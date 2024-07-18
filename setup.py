c334424fab2b7a32ae037f654c6495f38dc82d1f
Eason Zhang <yishen.zhang@kuleuven.be>
2024-07-18 00:52:32 -0500

    check updates

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ setuptools.setup(
     name="massbalance",
     version="v0.2",
     author="Yishen Zhang",
-    author_email="yishen.zhang@kuleuven.be",
+    author_email="yishen.zhang@rice.edu",
     packages= setuptools.find_packages(
         exclude= ['example', 'benchmark', 'Tutorial']
         ),
 
 
-    install_requires= [
-    'pandas',
-    'numpy',
-    'scipy',
-    ]
+    install_requires=[
+            'pandas>=1.3.5',
+            'numpy>=1.21.5',
+            'matplotlib>=3.5.1',
+            'scipy>=1.7.3',
+            ],
 )
